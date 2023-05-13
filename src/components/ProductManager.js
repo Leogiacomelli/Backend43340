dd718bb3c9b08aacf89de665bbdd235aed423463
Leandro Giacomelli <leandroegiacomelli@gmail.com>
2023-05-13 11:24:59 -0300

    Segund Commit

--- src/components/ProductManager.js
+++ src/components/ProductManager.js
@@ -1,6 +1,6 @@
 import { promises as fs } from "fs";
 
-class ProductManager {
+export default class ProductManager {
   constructor() {
     this.patch = "./productos.txt";
     this.products = [];
@@ -59,19 +59,37 @@ class ProductManager {
     await fs.writeFile(this.patch, JSON.stringify(productsModif));
   };
 }
-const productos = new ProductManager();
-
-/* productos.addProduct("Titulo1", "Description1", 2500, "Imagen1", "ds120", 3);
+//const productos = new ProductManager();
+/* 
+productos.addProduct("Titulo1", "Description1", 2500, "Imagen1", "ds120", 3);
 
 productos.addProduct("Titulo2", "Description2", 2700, "Imagen2", "ds125", 5);
 
-productos.addProduct("Titulo3", "Description3", 3500, "Imagen3", "ds123", 7); */
+productos.addProduct("Titulo3", "Description3", 3500, "Imagen3", "ds123", 7);
 
-///productos.getProducts();
+productos.addProduct("Titulo4", "Description4", 4000, "Imagen4", "ds124", 7);
+
+productos.addProduct("Titulo5", "Description5", 5000, "Imagen5", "ds125", 5);
+
+productos.addProduct("Titulo6", "Description6", 6000, "Imagen6", "ds126", 6);
+
+productos.addProduct("Titulo7", "Description7", 7000, "Imagen7", "ds127", 7);
+productos.addProduct("Titulo8", "Description8", 8000, "Imagen8", "ds128", 8);
+productos.addProduct("Titulo9", "Description9", 9000, "Imagen9", "ds129", 9);
+productos.addProduct(
+  "Titulo10",
+  "Description10",
+  10000,
+  "Imagen10",
+  "ds1210",
+  10
+); */
+
+//productos.getProducts();
 
 //productos.getProductsById(3);
 //productos.deleteProductsById(2);
-productos.updateProducts({
+/* productos.updateProducts({
   title: "Titulo3",
   description: "Description3",
   price: 5800,
@@ -79,4 +97,4 @@ productos.updateProducts({
   code: "ds123",
   stock: 7,
   id: 3,
-});
+}); */
